6ed941415c756da8bd653c2437e8e5c98cafd265
🥷r ️‍🔥 <sh31khrxh1m3@gmail.com>
2024-12-22 12:57:39 +0000

    Update game.py

--- game.py
+++ game.py
@@ -12,9 +12,45 @@ class Bin(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load("Lesson 7/images/bin.png")
-        self.image = pygame.transform.scale(self.image,(100,120))
+        self.image = pygame.transform.scale(self.image,(100,12))
         self.rect = self.image.get_rect()
         self.rect.center = x, y
+
+class Recycle(pygame.sprite.Sprite):
+    def __init__(self, x, y, img):
+        super().__init__()
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.rect.center = x, y
+
+
+class Non_Recycle(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("Lesson 7/images/bag.png")
+        self.image = pygame.transform.scale(self.image,(60,61))
+        self.rect = self.image.get_rect()
+        self.rect.center = x, y
+
+non_recyclegroup = pygame.sprite.Group()
+recyclegroup = pygame.sprite.Group()
+
+images = ["Lesson 7/images/pencil.png", "Lesson 7/images/cardboardbag.png", "Lesson 7/images/box.png"]
+
+
+for i in range(200):
+    x = random.randint(50,1750)
+    y = random.randint(50, 1150)
+    img = random.choice(images)
+    recycle = Recycle(x, y, img)
+    recyclegroup.add(recycle)
+
+for i in range(100):
+    x = random.randint(50,1750)
+    y = random.randint(50, 1150)
+    non_recycle = Non_Recycle(x, y)
+    non_recyclegroup.add(non_recycle)
+
 
 bin = Bin(WIDTH/2, HEIGHT/2)
 bingroup = pygame.sprite.Group()
@@ -27,4 +63,6 @@ while run:
             pygame.QUIT()
     screen.blit(bg,(0,0))
     bingroup.draw(screen)
+    recyclegroup.draw(screen)
+    non_recyclegroup.draw(screen)
     pygame.display.update()
